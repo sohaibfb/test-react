f15af28b7332d4534e58766299b59a65aed698ee
Sohaib <sohaib.f.bataineh@gmail.com>
2021-03-31 21:46:31 +0300

    31032021

---
+++ src/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import navlink from 'react-router-dom'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <navlink></navlink>
+        </div>
+
+        
+    );
+}
+
+export default LandingPage;
+
+
